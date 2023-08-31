07df75f1f0eea3536b7456ad4ae8fb8669a667a9
Logon Aluno <logonrmlocal@fiap.com.br>
2023-08-30 21:59:06 -0300

    Checkpoint 01 2SEM

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,4 +10,8 @@ module "compute" {
     ec2_lt_name          = "${var.ec2_lt_name}"
     ec2_lt_ami           = "${var.ec2_lt_ami}"
     ec2_lt_instance_type = "${var.ec2_lt_instance_type}"
+    vpc_cidr             = "${var.vpc_cidr}"
+    vpc_id               = "${module.network.vpc_id}"
+    vpc_sn_pub_az1_id    = "${module.network.vpc_sn_pub_az1_id}"
+    vpc_sg_pub_id        = "${module.network.vpc_sg_pub_id}"
 }
